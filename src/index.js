900172f470811c80659f05675c4facb2a7df2b8e
GrechkoseyNadezhda <Grechkosey.n@gmail.com>
2022-11-30 11:32:53 +0100

    pagination

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import ApiService from './js/apiService';
 import Movie from './js/movie';
 import MovieTemplate from './templates/movieTemplate.hbs';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+import {container, paginationSettings} from './js/pagination'
+import './js/movie'
+
 
 const refs = {
   movieListRef: document.querySelector('.movie-list'),
@@ -9,13 +14,19 @@ const refs = {
 
 refs.formRef.addEventListener('submit', onFormSubmit);
 
-const apiService = new ApiService();
+export const apiService = new ApiService();
 
-async function fillMovies() {
+async function fillMovies(page) {
   try {
     const {
-      data: { results },
+      data: { results, total_results },
     } = await apiService.getTrendingMovies();
+
+    initPagination({
+      page,
+      itemsPerPage: results.length,
+      totalItems: total_results
+    })
 
     const markup = await parseObjects(results);
 
@@ -25,7 +36,7 @@ async function fillMovies() {
   }
 }
 
-fillMovies();
+fillMovies(paginationSettings.startPage);
 
 async function onFormSubmit(e) {
   e.preventDefault();
@@ -57,4 +68,43 @@ async function parseObjects(arr) {
   } catch (err) {
     console.log(err);
   }
+}
+
+
+function initPagination ({ page, itemsPerPage, totalItems }) {
+  const options = {
+    totalItems,
+    itemsPerPage,
+    page,
+    visiblePages: 5,
+    centerAlign: false,
+    template: {
+      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      moveButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</a>',
+      disabledMoveButton:
+        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '</span>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+          '<span class="tui-ico-ellip">...</span>' +
+        '</a>'
+    }
+  };
+  const pagination = new Pagination(container, options);
+
+  paginationSettings.pagination = pagination;
+
+  pagination.on('afterMove', async ({ page }) => {
+    apiService.page = page
+
+    window.scroll(0,0);
+    fillMovies(page)
+
+  });
+
 }
---
+++ src/js/pagination.js
@@ -0,0 +1,11 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
+
+export const container = document.getElementById('pagination');
+export const paginationSettings = {
+  startPage: 1,
+  searchType: null,
+  pagination: null,
+  totalItemsHome: null,
+};
