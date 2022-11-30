5b3fd5a690215abc6e84c65e8f38c053d6c49dec
GrechkoseyNadezhda <Grechkosey.n@gmail.com>
2022-11-30 18:20:19 +0100

    watched movies

--- src/js/watched.js
+++ src/js/watched.js
@@ -1,48 +1,47 @@
 import ApiService from './apiService';
-import Movie from './movie';
 const library = document.querySelector('.watched')
 const WATCHED_FILM_KEY = 'watched-films';
-const filmInfo = [436270];
+const filmInfo = [436270, 668461];
 // 436270  668461  119051
 
 
 localStorage.setItem(WATCHED_FILM_KEY, JSON.stringify(filmInfo));
 
-
 const myLibraryApi = new ApiService();
 
-onWatchedFilmsShow()
-async function onWatchedFilmsShow() {
-  try {
+(async () => {
+    const watchedFilms = localStorage.getItem(WATCHED_FILM_KEY);
+    const parsedWatchedFilms = JSON.parse(watchedFilms);
+    if (watchedFilms) {
+      parsedWatchedFilms.forEach(async el => {
+        try {
+          const res = await myLibraryApi.getMovieById(el);
+          const movieById = res.data
+          const genres = movieById.genres.map(el => el.name).join(', ')
+          const date = new Date(movieById.release_date).getFullYear()
 
-    const movieId = filmInfo[0]
-    const res = await myLibraryApi.getMovieById(movieId);
-    const movieById = res.data
-    const genres = movieById.genres.map(el => el.name).join(', ')
-    const date = new Date(movieById.release_date).getFullYear()
-
-    const markup = `
-      <li class='movie' data-id='${movieById.id}'>
-      <img
-        class='movie__img'
-        width='440'
-        src='https://image.tmdb.org/t/p/original/${movieById.poster_path}'
-        alt='${movieById.title}'
-        loading='lazy'
-      />
-      <div class='movie__info'>
-        <p class='movie__name'>${movieById.title}</p>
-        <p class='movie__description'>${genres}
-          |
-          ${date}</p>
-      </div>
-      </li>
-    `
-
-    // library.innerHTML = markup;
-
-  } catch (err) {
-    console.log(err);
-  }
-}
+          const markup = `
+            <li class='movie' data-id='${movieById.id}'>
+            <img
+              class='movie__img'
+              width='440'
+              src='https://image.tmdb.org/t/p/original/${movieById.poster_path}'
+              alt='${movieById.title}'
+              loading='lazy'
+            />
+            <div class='movie__info'>
+              <p class='movie__name'>${movieById.title}</p>
+              <p class='movie__description'>${genres}
+                | ${date}</p>
+            </div>
+            </li>
+          `
+        library.insertAdjacentHTML('beforeend', markup)
+        } catch (err) {
+          console.log(err);
+        }
+      })
+    }
+  
+})();
 
