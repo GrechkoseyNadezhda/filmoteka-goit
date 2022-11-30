6014f54e3a2a770b31b8055b67fe6c18508607f8
AntonVitulskyi <vitulskiyanton@gmail.com>
2022-11-30 15:05:40 +0200

    Modal opening + backdrop closing + bug fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,5 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import ApiService from './apiService';
 
 const movieItemEl = document.querySelector('.movie-list')
@@ -11,11 +13,17 @@ const genresValueEl = document.querySelector('[data-genres]')
 const overviewValueEl = document.querySelector('[data-text-overview]')
 const modalWindowEl = document.getElementById("modal_window");
 const closeModalBtn = document.querySelector('.modal_close_btn')
+const modalBackDropToCloseEl = document.querySelector('.js-backdrop')
+const addToWatchedListBtn = document.querySelector('.js-toWatchBtn')
+const addToQueueListBtn = document.querySelector('.js-toAddtoQue')
 
 const onClickOpenModal = event => {
-    
+    Loading.standard({
+        clickToClose: true,
+        svgSize: '100px',
+        cssAnimationDuration: 0,
+        });
     const movieId = event.target.closest('li').getAttribute('data-id') 
-
     const newMovie = new ApiService();
     newMovie.getMovieById(movieId)
     .then(object => { console.log(object.data)
@@ -27,14 +35,24 @@ const onClickOpenModal = event => {
         overviewValueEl.textContent = object.data.overview;
         genresValueEl.textContent = object.data.genres.map(el => el.name).join(', ')
         imageEl.src = event.target.closest('IMG').src
+        modalWindowEl.showModal();
+
     })
-    .catch(err => console.log(err))
-    modalWindowEl.showModal();
+    .catch(err => Notify.info("Sorry, this movie is currently unavailable!", {  fontSize: '16px', width: '200px'},))
+    .finally(Loading.remove)
 }
 
 const onClickCloseModal = event => {
-  
-    modalWindowEl.close()
+        if(event.target.tagName === 'DIALOG' || event.target.closest(".modal_close_btn")){
+            modalWindowEl.close()  
+        } 
 }
+
+const addToWatchedList = event => console.log('Ooooooo');
+
+const addToQueueList = event => console.log('Aaaaaaa');
+
 movieItemEl.addEventListener('click', onClickOpenModal)
-closeModalBtn.addEventListener('click', onClickCloseModal)
+window.addEventListener('click', onClickCloseModal)
+addToWatchedListBtn.addEventListener('click', addToWatchedList)
+addToQueueListBtn.addEventListener('click', addToQueueList)
