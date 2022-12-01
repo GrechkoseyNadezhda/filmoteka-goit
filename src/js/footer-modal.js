caa9935e859b0088e24ff7b212d6ce5eb354e9a0
Oleksandr Bron <lexlutermotion@gmail.com>
2022-12-01 20:50:11 +0200

    added fix overflow to footer modal

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -2,10 +2,18 @@ openFooterModal = document.querySelector('.footer-section__link');
 closeFooterModalBtn = document.querySelector('.footer-modal__btn');
 footerModal = document.querySelector('[data-footerModal]');
 
-openFooterModal.addEventListener('click', toggleModal);
-closeFooterModalBtn.addEventListener('click', toggleModal);
+openFooterModal.addEventListener('click', onOpenFooterModalClick);
+closeFooterModalBtn.addEventListener('click', onCloseFooterModalClick);
 
-function toggleModal(e) {
+
+function onOpenFooterModalClick(e) {
     e.preventDefault();
-    footerModal.classList.toggle('is-hidden');
+    footerModal.classList.remove('is-hidden');
+    document.body.classList.add('overflow');
+}
+
+function onCloseFooterModalClick(e) {
+    e.preventDefault();
+    footerModal.classList.add('is-hidden');
+    document.body.classList.remove('overflow');
 }
