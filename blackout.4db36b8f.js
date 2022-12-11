7cf42805f72139b6ba867aa1a09588f5d8896fce
GrechkoseyNadezhda <89804832+GrechkoseyNadezhda@users.noreply.github.com>
2022-12-11 09:45:02 +0000

    Deploying to gh-pages from @ GrechkoseyNadezhda/filmoteka-goit@3e14c2d2548194197eeb7d1224edf1225d3f383f 🚀

---
+++ blackout.4db36b8f.js
@@ -0,0 +1,2 @@
+function e(e){const t=e.clientX||e.touches[0].clientX,n=e.clientY||e.touches[0].clientY;document.documentElement.style.setProperty("--cursorX",t+"px"),document.documentElement.style.setProperty("--cursorY",n+"px")}document.addEventListener("mousemove",e),document.addEventListener("touchmove",e);
+//# sourceMappingURL=blackout.4db36b8f.js.map
---
+++ blackout.d22b11c0.js
@@ -0,0 +1,2 @@
+!function(){function e(e){var t=e.clientX||e.touches[0].clientX,n=e.clientY||e.touches[0].clientY;document.documentElement.style.setProperty("--cursorX",t+"px"),document.documentElement.style.setProperty("--cursorY",n+"px")}document.addEventListener("mousemove",e),document.addEventListener("touchmove",e)}();
+//# sourceMappingURL=blackout.d22b11c0.js.map
